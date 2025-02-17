7e125425141a64ea4e10cff0309fe43547005e34
vassyusha <romanoffmiss@yandex.ru>
2025-02-17 15:56:40 +0300

    change in struct

--- include/polynomial.h
+++ include/polynomial.h
@@ -1,1 +1,29 @@
+#pragma once
 
+#include <string>
+#include <iostream>
+
+class Monomial {
+private:
+
+	double score = 0.;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+public:
+
+	Monomial(double score = 0., int x = 0, int y = 0, int z = 0) : score(score), x(x), y(y), z(z) {}
+
+	bool operator==(const Monomial& other) {
+
+	}
+
+};
+
+class Polinomial {
+private:
+
+public:
+
+};
